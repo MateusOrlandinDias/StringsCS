0cdffac18eb3d11015951ba1a69954f1b8127b93
Mateus Orlandin Dias <mateus.dias@meta.com.br>
2022-12-21 20:41:23 -0300

    Guid Example

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StringsCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid();
+            id.ToString().Substring(0, 8);
+
+            id = new Guid("c058af95-25b6-4f4e-93e4-74ef94f6424c");
+            Console.WriteLine(id);
+        }
+    }
+}
