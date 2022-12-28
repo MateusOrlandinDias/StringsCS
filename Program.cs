08f1682622a4b6a3405a32b2c0bf3a2404d9782e
Mateus Orlandin Dias <mateus.dias@meta.com.br>
2022-12-28 20:35:54 -0300

    Metodos strings

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace StringsCS
 {
@@ -6,16 +7,38 @@ namespace StringsCS
     {
         static void Main(string[] args)
         {
-            var price = 10.2+19.2;
-            // var text = "O preço do produto é "+price+" apenas na promoção";
-            // var text = string.Format("O preço do produto é {0} apenas na promoção {1}.", price, true);
-            var text = $"O preço do produto é {price} apenas na promoção.";
-            // var text = $@"O preço do produto 
-            // é {price} apenas 
-            // na promoção.";
-            // var text = $"O preço do produto \n é {price} apenas na promoção.";
+            var texto = "Este texto é um teste";
+            // var num = 33;
+            // Console.WriteLine(texto.CompareTo("testando"));
+            // Console.WriteLine(texto.Contains("Testando"));
+            // Console.WriteLine(texto.Contains("testando", StringComparison.OrdinalIgnoreCase));
+            // Console.WriteLine(texto.EndsWith("Teste", StringComparison.OrdinalIgnoreCase));
+            // Console.WriteLine(texto.StartsWith("este ", StringComparison.OrdinalIgnoreCase));
+            // Console.WriteLine(texto.Equals("Este texto é um Teste", StringComparison.OrdinalIgnoreCase));
+            // Console.WriteLine(texto.Equals("Este texto é um Teste"));
+            // Console.WriteLine(num.Equals(33));
+            // Console.WriteLine(texto.LastIndexOf("te"));
+            // Console.WriteLine(texto.IndexOf("texto"));
+            // Console.WriteLine(texto.ToLower());
+            // Console.WriteLine(texto.ToUpper());
+            // Console.WriteLine(texto.Insert(5, "AQUI "));
+            // Console.WriteLine(texto.Remove(5, 5));
+            // Console.WriteLine(texto.Length);
+            // Console.WriteLine(texto.Replace("Este", "Isto"));
+            // var divisao = texto.Split(" ");
+            // Console.WriteLine(divisao[1]);
+            // Console.WriteLine(texto.Substring(5, texto.LastIndexOf("o")));
+            // Console.WriteLine(texto.Trim());
+            // texto+=" aqui";
+            // Console.WriteLine(texto);
+            //PARA TEXTOS MUITO GRANDES USAR O STRING BUILDER
+            // var textoGrande = new StringBuilder();
+            // textoGrande.Append("Este texto é um teste"); 
+            // textoGrande.Append("Este texto é umte"); 
+            // textoGrande.Append("Eexto é um teste"); 
+            // textoGrande.Append("Este texto é um testestesteste"); 
 
-            Console.WriteLine(text);
+            // Console.WriteLine(textoGrande.ToString());
         }
     }
 }
